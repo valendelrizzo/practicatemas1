bfb93a934f5ca8d04750f4cf6b0f9e8555caf2a8
valendelrizzo <vdelrizzo@frba.utn.edu.ar>
2023-07-27 21:47:06 -0300

    ejercicio de cantidad de empleados terminado

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,37 @@
 module Library where
 import PdePreludat
+import Data.String (IsString)
+
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+triple :: Number -> Number
+triple numero = numero *3
+
+letraMenorMayor :: String -> Bool
+letraMenorMayor nombreEmpresa
+    |last nombreEmpresa < head nombreEmpresa = True
+    |otherwise = False
+
+letrasIntermedias :: String -> Number
+letrasIntermedias  nombreEmpresa = length nombreEmpresa - 2
+
+nombreCapicua :: String -> Bool
+nombreCapicua nombreEmpresa
+    |reverse nombreEmpresa == nombreEmpresa = True
+    |otherwise = False
+
+dobleCantidadLetras :: String -> Number
+dobleCantidadLetras nombreEmpresa = 2*length nombreEmpresa
+
+esDivisible :: Number -> Number -> Bool
+esDivisible nombreEmpresa tresOsiete = mod nombreEmpresa tresOsiete == 0
+
+cantidadEmpleados :: String -> Number
+cantidadEmpleados nombreEmpresa
+    |nombreEmpresa == "Acme" = 10
+    |letraMenorMayor nombreEmpresa = letrasIntermedias nombreEmpresa
+    |nombreCapicua nombreEmpresa = dobleCantidadLetras nombreEmpresa
+    |esDivisible (length nombreEmpresa) 3 || esDivisible (length nombreEmpresa) 7 = 3
+    |otherwise = 0
